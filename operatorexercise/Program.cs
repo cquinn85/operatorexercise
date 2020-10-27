2f6bb3fba1dca18f80aae920171d6f0d7f700f70
cquinn85 <cequinn300@gmail.com>
2020-10-27 17:08:36 -0500

    operator exercise

---
+++ operatorexercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operatorexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}.");
+
+            var r = double.Parse(Console.ReadLine());
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}.");
+
+        }
+    }
+}
